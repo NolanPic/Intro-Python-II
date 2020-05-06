563836df5005970f6fab8b62c7a5cfb1c6ac0a1e
Nolan Picini <nolanpic@gmail.com>
2020-05-06 14:25:08 -0700

    Implement beginning game loop

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,6 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,9 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print(f"You are here: {player.current_room.name}")
+    cmd = input("Command: ")
+    if cmd == 'q':
+        break
